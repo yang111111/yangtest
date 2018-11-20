d8850aa13b59fd05aa1e690dea851c7fea8dc7d1
asus <asus@DESKTOP-8AGLPBD>
2018-11-20 16:47:39 +0800

    更新test

--- src/main/java/som/dong/testgit/Test.java
+++ src/main/java/som/dong/testgit/Test.java
@@ -1,14 +1,19 @@
 package som.dong.testgit;
-/** * @author  作者 E-mail: * 
-@date 创建时间：2018年11月20日 下午4:42:39 * 
-@version 1.0 * 
-@parameter  * 
-@since  * 
-@return  */
+
+/**
+ * * @author 作者 E-mail: *
+ * 
+ * @date 创建时间：2018年11月20日 下午4:42:39 *
+ * @version 1.0 *
+ * @parameter *
+ * @since *
+ * @return
+ */
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("用来测试数据用的");
 
 	}
 
